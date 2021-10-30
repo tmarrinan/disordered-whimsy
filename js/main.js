84c6f1028230a7c20ffa476db22bd30d57719ec4
Thomas Marrinan <thomas.j.marrinan@gmail.com>
2021-10-30 07:36:20 -0500

    update nav (disappear when scrolling)

--- js/main.js
+++ js/main.js
@@ -5,6 +5,7 @@ function init() {
     // adjust main content spacing
     adjustMainContentPadding();
     $(window).resize(adjustMainContentPadding);
+    $(window).scroll(adjustHeaderVisibility);
 }
 
 function adjustMainContentPadding() {
@@ -12,4 +13,20 @@ function adjustMainContentPadding() {
     let desktop_nav_height = $("#headerDesktop").height();
     let nav_height = Math.max(mobile_nav_height, desktop_nav_height);
     $("#main").css("padding-top", nav_height + "px");
+}
+
+function adjustHeaderVisibility() {
+    let scroll_top = $(window).scrollTop();
+    let mobile_nav_height = $("#headerMobile").height();
+    let desktop_nav_height = $("#headerDesktop").height();
+    let nav_height = Math.max(mobile_nav_height, desktop_nav_height);
+    if (scroll_top > nav_height) {
+        let diff = nav_height - scroll_top;
+        $("#headerMobile").css("top", diff + "px");
+        $("#headerDesktop").css("top", diff + "px");
+    }
+    else {
+        $("#headerMobile").css("top", "0px");
+        $("#headerDesktop").css("top", "0px");
+    }
 }
